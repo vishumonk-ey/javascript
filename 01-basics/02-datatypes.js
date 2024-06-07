461966cd95216296152c2eed86557cff1522eafa
vishumonk-ey <vkant2005@gmail.com>
2024-06-07 11:39:48 +0000

    conversion of data types

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,18 @@
+"use script"/* all of our code will be treated as a newer version of js */
+// alert(3+3); not defined in nodejs but will work in browser
+let name= "vishal"
+let age=19
+let isLoggedIn= true
+// data types
+// number- range
+// bigInt- bhot bada number rkh
+// boolean-true/false
+// string- "" ''
+// null- standalone value but is an object datatype 
+// undefined
+// symbol- unique 
+// object
+console.log(typeof age)
+console.log(typeof "mitthu")
+console.log(typeof null);  // object data type 
+console.log(typeof undefined);//undefined data type
---
+++ 01-basics/03-ConversionOperator.js
@@ -0,0 +1,22 @@
+let score = true
+console.log(typeof score);
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33"=> 33
+// "33avs"=> NaN
+// "true"-1/ "false"-0
+// null - 0
+
+let isLoggedIn="vishal"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//  0 - false 
+// 1 and other no.s - truee
+// ""     - false
+// "vishuuu"   - true
+
+let someNumber=undefined
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
