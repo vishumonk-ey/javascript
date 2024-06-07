094fb96c2dbea2b074ac1ef746ca6e0f9561389f
vishumonk-ey <vkant2005@gmail.com>
2024-06-07 08:40:35 +0000

    container config done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,10 @@
+let accountName="vishal";
+const accountId=12345;
+var accountCity="delhi";
+accountEmail="vishal@gmail.com"
+let accountState;
+console.log(accountName);
+console.table([accountName,accountId,accountCity,accountEmail,accountState]);
+
+/* dont use var for variable declaration as it consists block error (curly braces error)*/
+// use const for constant declaration and let for var variable
