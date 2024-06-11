ede815162248469a49152d7941ca25aea0fc5fc4
vishumonk-ey <vkant2005@gmail.com>
2024-06-11 05:30:07 +0000

    array done

--- 02-basics/02-array.js
+++ 02-basics/02-array.js
@@ -10,6 +10,9 @@ console.log(myHero.concat(genZhero))
 let nonUsableArray=[1,2,3,[4,5,6],[[7,8,9]]]
 console.log(nonUsableArray.flat(2)) /* to remove array ke andar array and give depth also ki kitne andar tk ke array ko hatana haiiii */
 console.log(Array.isArray(false));
+
+// is array only returns true when array constructor ( newArray()) and empty brackets se declare kiya ho!!
+
 console.log(Array.from('vishal'));
 console.log(Array.from({name:"vishal",})); /*will return an empty string because vo confused hai ki value ka array banana hai ya name ka ------ interview important*/ 
 let score1="10"
