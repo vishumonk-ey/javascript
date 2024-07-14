011d2b884c86aab5069ce4a0a8fcb2242b04a01f
vishumonk-ey <2023etb053.vishal@students.iiests.ac.in>
2024-07-14 23:48:54 +0530

    made a basic calculator

---
+++ js/calculator/script.js
@@ -0,0 +1,39 @@
+const allClear=document.getElementById("allClear")
+const delBtn=document.getElementById("delete")
+const arithOp=document.getElementById("arithOp")
+const calculate=document.getElementById("calculate")
+const inputDisplay=document.querySelector(".inputDisplay")
+let allowArithOp=false
+const addInput=function(e){
+    if(e.target.id=="arithOp"){
+        if(allowArithOp){
+            inputDisplay.innerHTML+=e.target.innerHTML
+        }
+    }else if(e.target.id=="allClear"){
+        allowArithOp=false
+        inputDisplay.innerHTML=""
+    }else if(e.target.id=="delete"){
+        const userInput=inputDisplay.innerHTML
+        inputDisplay.innerHTML=userInput.slice(0,userInput.length - 1)
+        if(inputDisplay.innerHTML.length===0){
+            allowArithOp=false
+        }
+    }else if(e.target.id=="calculate"){
+        calculateFunc(e)
+    }else{
+        console.log("fireddd");
+        inputDisplay.innerHTML+=e.target.innerHTML
+        allowArithOp=true
+    }
+}
+document.querySelector(".container").addEventListener
+('click',function(e){
+    if(e.target.tagName="P"){
+        console.log("fired");
+        addInput(e)
+    }
+},false)
+function calculateFunc(e){
+    console.log("calc fired");
+    inputDisplay.innerHTML=`${(inputDisplay.innerHTML)}`
+}
