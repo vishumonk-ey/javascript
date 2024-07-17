3b07eb91c642a9265ca5f3dc5c5800ec710bed3f
vishumonk-ey <2023etb053.vishal@students.iiests.ac.in>
2024-07-17 09:33:52 +0000

    learnt difference between date.now() and new date()

--- 01-basics/07-dateAndTime.js
+++ 01-basics/07-dateAndTime.js
@@ -3,7 +3,13 @@
 const newDate= new Date()
 // console.log(newDate.toString());
 // console.log(newDate.toLocaleString());
+// console.log(Date.now().toLocaleString());
+// Date.now().toLocaleString krkee mereko epoch se time mil rha hai but new Date() krke vo acche se date and time de rha hai
 // console.log(newDate.toDateString());
+// console.log(typeof Date.now());
+// date.now()-->number data type 
+// new Date()-->object data type 
+// console.log(typeof new Date());
 // console.log(newDate.toISOString());
 // console.log(newDate.toJSON());
 // console.log(newDate.toLocaleTimeString());
@@ -17,8 +23,8 @@ const myNewDate= new Date("2024-01-13")
 // console.log(myNewDate.toDateString());
 
 const newDateOne=new Date()
-console.log(newDateOne.toLocaleString('default',{
-    weekday:'long',
-}));
+// console.log(newDateOne.toLocaleString('default',{
+//     weekday:'long',
+// }));
 
 // we can customize the way we want our date in locale string.
