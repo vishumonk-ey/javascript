691f8b057b7bb5827f590216e3ba43becbdfbd55
vishumonk-ey <2023etb053.vishal@students.iiests.ac.in>
2024-07-29 07:04:44 +0000

    added notes

--- 02-basics/04-object.js
+++ 02-basics/04-object.js
@@ -5,7 +5,7 @@ tinderUser.id="123abc"
 tinderUser.name="vishaaaaal"
 tinderUser.isLoggedIn=true
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 const user={
     name:{
@@ -17,6 +17,7 @@ const user={
 }
 
 // console.log(user.name.userfullname.firstname);
+// console.log(user.firstname);
 
 const obj1={1:"a" ,
             2:"b",
@@ -26,10 +27,14 @@ const obj2={3:"a" ,
 }
 // let obj3={obj1,obj2}  obj1 and obj2 key hogya and value hogyi
 const total=Object.assign({},obj1,obj2) 
+const totalNew= Object.assign(obj1,obj2)
+// target object ko bhi badal diya jaa rha hai 
+// console.log(totalNew);
+// console.log("obj1",obj1);
 /* assign mera dono objects ko merge kr dega and first vala mera target hai aur baaki source hainnn ... target me source ko merge kr diya jayega and in end mera total aur target dono same honge*/
-console.log(total);
-console.log(obj1);
-console.log(obj2);
+// console.log(total);
+// console.log(obj1);
+// console.log(obj2);
 
 // const obj3={...obj1,...obj2} spread operator..
 
@@ -44,7 +49,9 @@ const course={
     name:'JS in Hindi',
     courseInstructor:'Hitesh',
 }
-const {courseInstructor: inst}=course
-
-console.log(inst)
+// const se object declare krdiya means the refference of the object is fixed. vapas se course naam se kuch nhi declare kr sktA
+let {courseInstructor: inst}=course
+let {name: naam} = course
+console.log(naam);
+console.log(inst);
 
