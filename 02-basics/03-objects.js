91c07b710484fc4d77e3ba2ab07dbd3d8e346369
vishumonk-ey <2023etb053.vishal@students.iiests.ac.in>
2024-07-29 05:12:08 +0000

    added some notes and revised

--- 02-basics/03-objects.js
+++ 02-basics/03-objects.js
@@ -5,20 +5,28 @@
 
 // object literals
 const mySym= Symbol("key1")
+// symbol data type even though same description but still both of em are different
+const mySymNew= Symbol("key1")
 const JsUser={
     name:"Vishal",
     age:19,
+    [mySymNew]:"jojoj",
     [mySym]:"pepepe", /* symbol declaration in object syntax INTERVIEW*/
     "full name" : 'Vishal Kant',
     email: "vkant2005@gmail.xom",
     greetingTwo:function(){
         console.log(`hello ${this["full name"]}`)
+    },
+    nickName:function(){
+        console.log(`my fullname is ${this["full name"]}`);
     }
+
 }
-console.log(JsUser["full name"]);
-console.log(JsUser.age);
-console.log(JsUser["age"]);
-console.log(JsUser[mySym])
+// console.log(JsUser[mySymNew]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser.age);
+// console.log(JsUser["age"]);
+// console.log(JsUser[mySym])
 // keys are generally stored as string in system prr hamne symbol ko bhi key liya !!!!1
  JsUser.age=18;
 //  Object.freeze(JsUser)  ----- now value wont be changed
@@ -27,3 +35,4 @@ JsUser.greeting=function (){
 }
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+console.log(typeof mySym);
