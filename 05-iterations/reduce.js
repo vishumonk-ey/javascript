471a4daf7789e119276c6039b91d34ed3993006f
vishumonk-ey <2023etb053.vishal@students.iiests.ac.in>
2024-12-15 20:54:14 +0530

    added notes on reduce methof

--- 05-iterations/reduce.js
+++ 05-iterations/reduce.js
@@ -36,5 +36,7 @@ const shoppingCart=[
  return acc+currval.price
 },0)
 console.log(total);
-
-// reduce is used for summing up value in arrays
+// if there is no accumulator val specified then the first element becomes the accumulator value and the callback function start from 2nd element.
+// if there is only one elmnt in array and no initial value specified then the 1st elmnt will become the accumulator value but there wont be any current value so no cb calling
+// if there is an empty array and an initial value specified still no cb calling and if no initital value then type error.
+// reduce is used for summing up value in arrays but can be used for more than this
